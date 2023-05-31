7c0dc38ec646684718a87df5672fa5ccf4ccec0f
jose.tunon@itxpertix.com <jose.tunon@itxpertix.com>
2023-05-31 11:41:35 -0500

    Reconfiguracion de variables de entorno

--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,5 @@
 module.exports = {
-  inventory: {    
-    connectionLimit : 1000,
-    connectTimeout  : 10000,
-    acquireTimeout  : 10000,
-    timeout         : 10000,
+  inventory: {
     host: "containers-us-west-132.railway.app",
     user: "root",
     password: "k8dCtvYdLFtdKTSdVCOk",

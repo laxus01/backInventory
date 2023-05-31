4e21e1d35dbd86d9e045130b26a95a44b30434a3
jose.tunon@itxpertix.com <jose.tunon@itxpertix.com>
2023-05-31 12:24:20 -0500

    prueba del servicio 2

--- src/models/user-model.js
+++ src/models/user-model.js
@@ -11,13 +11,31 @@ const token = jwt.sign(payload, llave.llave, {
 
 const getUser = async (req, res) => {
   const { login, password } = req.body;
-  return res.status(200).send({
-    res: {
-      message: "Autenticación correcta",
-      login: login,
-      pass: password
-    },
-  });
+  db.query(
+    "SELECT * FROM users WHERE user = ? AND password = ?",
+    [login, password],
+    (err, rows) => {
+      if(err) console.log(err);
+      if (err)
+        return res
+          .status(500)
+          .send({ respuesta: "Error al consultar el usuario" });
+          
+
+      if (rows.length === 0)
+        return res
+          .status(404)
+          .send({ respuesta: "Usuario o contraseña incorrectos" });
+
+      return res.status(200).send({
+        res: {
+          message: "Autenticación correcta",
+          token: token,
+          name: rows[0].name
+        },
+      });
+    }
+  );
 };
 
 module.exports = {

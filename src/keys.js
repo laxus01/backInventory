b94e817b9f33e28db7dd0e097abcff02c4f4e060
jose.tunon@itxpertix.com <jose.tunon@itxpertix.com>
2023-05-31 11:26:00 -0500

    configuracion de timeout

--- src/keys.js
+++ src/keys.js
@@ -1,5 +1,9 @@
 module.exports = {
-  inventory: {
+  inventory: {    
+    connectionLimit : 1000,
+    connectTimeout  : 60 * 60 * 1000,
+    acquireTimeout  : 60 * 60 * 1000,
+    timeout         : 60 * 60 * 1000,
     host: "containers-us-west-132.railway.app",
     user: "root",
     password: "k8dCtvYdLFtdKTSdVCOk",

587d3ea71e0b36673f5a8ff592be855cac3166b0
jose.tunon@itxpertix.com <jose.tunon@itxpertix.com>
2023-06-02 16:51:06 -0500

    politicas de cors

--- src/index.js
+++ src/index.js
@@ -23,6 +23,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+
 // Routes
 app.use(productsPath, require("./routes/products-route"));
 app.use(suppliersPath, require("./routes/suppliers-route"));

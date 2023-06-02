8db5da5a3c841fedb96183d76c4b25adb2bda413
jose.tunon@itxpertix.com <jose.tunon@itxpertix.com>
2023-06-02 15:34:25 -0500

    ROLLBACK

--- src/models/products-model.js
+++ src/models/products-model.js
@@ -4,18 +4,10 @@ const saveProduct = async (req, res) => {
   const { id, detail, sale_value, existence, barcode  } = req.body;
   const register_date = null;
 
-  const newProduct = {
-    id,
-    detail,
-    sale_value,
-    existence,
-    register_date,
-  };
+  
 
   db.query(
-    "INSERT INTO products set ?",
-    [newProduct],
-    (err, productsStored) => {
+    "INSERT INTO products VALUES (id, detail, sale_value, existence) (?, ?, ?, ?) ", [id, detail, sale_value, existence], (err, productsStored) => {
       if (err)
         return res
           .status(500)

cf523d3658b2feae99f48ad0e2f2f8e62c968d4e
jose.tunon@itxpertix.com <jose.tunon@itxpertix.com>
2023-06-02 15:11:51 -0500

    New base de datos

--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,9 @@
 module.exports = {
   inventory: {
-    host: "containers-us-west-100.railway.app",
+    host: "containers-us-west-126.railway.app",
     user: "root",
-    password: "dBm1KL0knE9dAIEHMOpf",
+    password: "xyVqpNDibXG2Z75J5pkT",
     database: "railway",
-    port: 7792
+    port: 6135
   },
 };
